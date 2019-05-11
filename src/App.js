06eede8e14579a0796c6e96b570e021954732eeb
Deary <chickenflakes9@gmail.com>
2019-05-11 13:58:03 +0700

    adding bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Contact from './components/Contact';
 import Header from './components/Header';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component  {
@@ -8,8 +10,10 @@ class App extends Component  {
     return (
       <div className="App">
         <Header branding="Contact Rocks" />
-        <Contact name="Ketibansapi" email="ketibs@gmail.com" phone="555-555-555" />
-        <Contact name="Chicken" email="chickenflakes@gmail.com" phone="555-555-555" />
+        <div className="container">
+          <Contact name="Ketibansapi" email="ketibs@gmail.com" phone="555-555-555" />
+          <Contact name="Chicken" email="chickenflakes@gmail.com" phone="555-555-555" />
+        </div>
       </div>
     );
   }
